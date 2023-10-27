2b525f463270197d8f98c41aac977d3bb2ad83d9
yagmurcandan <yagmur.hafizoglu@gmail.com>
2023-10-27 21:02:54 +0300

    LoadingBar

---
+++ script.js
@@ -0,0 +1,18 @@
+const counter = document.querySelector('.counter');
+const loadingBarFront = document.querySelector('.loadingBarFront');
+
+let number = 0;
+
+updateNumber();
+
+function updateNumber(){
+    counter.textContent = number + '%';
+    loadingBarFront.style.width = number + '%';
+    number++;
+
+    if(number<101){
+        setTimeout(updateNumber,15);
+    }
+}
+
+
